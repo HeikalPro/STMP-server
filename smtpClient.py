a292eab141305b8b94433dc3328fb40036787228
Ahmed Heikal <149324079+HeikalPro@users.noreply.github.com>
2024-06-27 19:35:23 +0300

    Add files via upload

---
+++ smtpClient.py
@@ -0,0 +1,72 @@
+from socket import *
+
+
+def smtp_client(port=1025, mailserver='127.0.0.1'):
+    msg = "\r\n My message"
+    endmsg = "\r\n.\r\n"
+
+
+    username = 'your_email@example.com'
+    password = 'your_password'
+
+    # Email details
+    from_addr = 'your_email@example.com'
+    to_addr = 'recipient@example.com'
+    subject = 'Test Email'
+    body = 'This is a test email.'
+    
+    # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
+    mail_server = mailserver
+    # Create socket called clientSocket and establish a TCP connection with mailserver and port
+    port = port
+    # Fill in start
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((mail_server, port))
+    # Fill in end
+
+    recv = clientSocket.recv(1024).decode()
+    print(recv) #You can use these print statement to validate return codes from the server.
+    if recv[:3] != '220':
+        print('220 reply not received from server.')
+
+    # Send HELO command and print server response.
+    heloCommand = 'HELO Alice\r\n'
+    clientSocket.send(heloCommand.encode())
+    recv1 = clientSocket.recv(1024).decode()
+    print(recv1) 
+    if recv1[:3] != '250':
+        print('250 reply not received from server.')
+
+    # Send MAIL FROM command and handle server response.
+    # Fill in start
+    mailFrom_coomand = f" Mail from : <{to_addr}\r\t>"
+    clientSocket.send(mailFrom_coomand.encode())
+    recv2 = clientSocket.recv(1024).decode()
+    print("Message aftr MAIL from command", recv2)
+    if recv2[:3] != "250":
+        print("250 reply not recived from server")
+    # Fill in end
+
+    # Send RCPT TO command and handle server response.
+    # Fill in start
+    # Fill in end
+
+    # Send DATA command and handle server response.
+    # Fill in start
+    # Fill in end
+
+    # Send message data.
+    # Fill in start
+    # Fill in end
+
+    # Message ends with a single period, send message end and handle server response.
+    # Fill in start
+    # Fill in end
+
+    # Send QUIT command and handle server response.
+    # Fill in start
+    # Fill in end
+
+
+if __name__ == '__main__':
+    smtp_client(1025, '127.0.0.1')
