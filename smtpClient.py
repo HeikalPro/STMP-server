89b46cb25c495f732cd755f93e9ab5949e7ba93f
Ahmed Heikal <149324079+HeikalPro@users.noreply.github.com>
2024-06-27 19:50:20 +0300

    smtpClient.py

--- smtpClient.py
+++ smtpClient.py
@@ -4,28 +4,28 @@ from socket import *
 def smtp_client(port=1025, mailserver='127.0.0.1'):
     msg = "\r\n My message"
     endmsg = "\r\n.\r\n"
-    
+
     # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
-    mail_server = mailserver
+
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
-    port = port
+
     # Fill in start
     clientSocket = socket(AF_INET, SOCK_STREAM)
-    clientSocket.connect((mail_server, port))
+    clientSocket.connect((mailserver, port))
     # Fill in end
 
     recv = clientSocket.recv(1024).decode()
-    print(recv) #You can use these print statement to validate return codes from the server.
-    if recv[:3] != '220':
-        print('220 reply not received from server.')
+    #print(recv) #You can use these print statement to validate return codes from the server.
+    #if recv[:3] != '220':
+    #    print('220 reply not received from server.')
 
     # Send HELO command and print server response.
     heloCommand = 'HELO Alice\r\n'
     clientSocket.send(heloCommand.encode())
     recv1 = clientSocket.recv(1024).decode()
-    print(recv1) 
-    if recv1[:3] != '250':
-        print('250 reply not received from server.')
+    #print(recv1) 
+    #if recv1[:3] != '250':
+    #    print('250 reply not received from server.')
 
     # Send MAIL FROM command and handle server response.
     # Fill in start
